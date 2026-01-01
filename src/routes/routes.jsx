9f4593ae64beaf3bf7a6b669bd53ee616bd038f5
NokibReja <nakib7reja@gmail.com>
2026-01-01 23:06:17 +0600

    first commit

---
+++ src/routes/routes.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../layout/MainLayout";
+
+
+const router = createBrowserRouter([
+    {
+        path:'/',
+        element: <MainLayout></MainLayout>
+    }
+])
